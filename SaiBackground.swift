70368b94c18af995e6d6978ebdeb673a93baefcf
saip9 <89651922+saip9@users.noreply.github.com>
2022-04-18 10:58:02 -0500

    Add files via upload

---
+++ SaiBackground.swift
@@ -0,0 +1,58 @@
+import Scenes
+import Igis
+
+  /*
+     This class is responsible for rendering the background.
+   */
+
+
+class Background : RenderableEntity {
+
+      init() {
+
+           // Using a meaningful name can be helpful for debugging
+          super.init(name:"Background")
+
+      }
+
+
+      override func setup(canvasSize:Size, canvas:Canvas) {
+          var questions = ["What is 1+1?", "What is the first planet?", "Who is the first president?"]
+          var answers = ["3, 11, 2, 4, Mercury, Earth, Venus, Uranus Gallo, Washington, Adams, Jeferson"];
+          var correctAnswer = ["2, Mercury, Washingtion"]
+          var score: Int = 0;
+
+
+
+
+
+    let  text = Text(location:Point(x:500, y:50), text:"Welcome to the KQUIZ Game")
+           text.font = "50pt times-new-roman"
+ 
+    let backgroundColorRect = Rect(topLeft:Point(x:0, y:0), size:Size(width:4000, height:2000))
+           let backgroundColorrectangle = Rectangle(rect:backgroundColorRect)
+           let backgroundColorFillStyle = FillStyle(color:Color(.antiquewhite))
+           let backgroundColorFillStyle2 = FillStyle(color:Color(.black))
+           let backgroundColorStrokeStyle = StrokeStyle(color:Color(.red))
+
+           canvas.render(backgroundColorFillStyle,  backgroundColorrectangle)
+           canvas.render(backgroundColorFillStyle2,backgroundColorStrokeStyle,text)
+
+           let gameRect =  Rect(topLeft:Point(x:450, y:200), size:Size(width:1000, height:600))
+           let gameRectangle = Rectangle(rect:gameRect)
+           let gameRectangleFillStyle = FillStyle(color:Color(.white))
+           let gameRectangleStrokeStyle = StrokeStyle(color:Color(.red))
+           let lineWidth = LineWidth(width:5)
+           canvas.render(gameRectangleFillStyle, gameRectangleStrokeStyle, lineWidth,gameRectangle)
+           
+           
+          
+//canvas.render(text)
+
+
+
+
+}
+}
+
+
