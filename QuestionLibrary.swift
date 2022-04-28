7287672203f976662f72c3f11ccd1441da6c01ac
saip9 <89651922+saip9@users.noreply.github.com>
2022-04-28 09:45:58 -0500

    QuestionLibrary -- to add questions into the bank

---
+++ QuestionLibrary.swift
@@ -0,0 +1,9 @@
+let quizQuestions = [
+    QuizQuestion(question: "What is 500+690?",
+                 answers: ["1190","1095", "987", "1290"], correctAnswerIndex: 0),
+    QuizQuestion(question: "What is the first planet?",
+                 answers: ["Mercury"," Earth", "Venus", "Uranus"], correctAnswerIndex: 0),
+    QuizQuestion(question:  "Who is the first president?",
+                 answers: ["Gallo", "Washington", "Adams", "Jefferson"], correctAnswerIndex:1)
+]
+
