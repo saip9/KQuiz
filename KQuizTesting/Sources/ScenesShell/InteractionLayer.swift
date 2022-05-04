380923f0889fcf31c54d868e6a286a8aeeaed5ad
sri-nagasai-siva-paladugu <srinagasaisiva.paladugu@student.allenisd.org>
2022-05-04 10:50:27 -0500

    this is everything

---
+++ KQuizTesting/Sources/ScenesShell/InteractionLayer.swift
@@ -0,0 +1,58 @@
+
+import Igis
+import Scenes
+import ScenesControls
+
+  /*
+     This class is responsible for the interaction Layer.
+     Internally, it maintains the RenderableEntities for this layer.
+   */
+
+
+class InteractionLayer : Layer {
+
+
+    init() {
+        // Using a meaningful name can be helpful for debugging
+        super.init(name:"Interaction")
+
+
+        // Create a standalone button, insert into LAYER, and assign handler
+        let independentButton = Button(name:"Alone", labelString:"Next", topLeft:Point(x:1150, y:550))
+        insert(entity:independentButton, at:.front)
+    independentButton.clickHandler = onButtonClickHandler
+
+        // Create two panels, one vertical and one horizontal
+    }
+
+
+    // Create a labeled panel with several buttons
+        // Create labels and buttons for panel and insert into PANEL
+      
+    
+    // Labels can respond to clicks but usually don't
+
+    // Buttons are generally only useful if they do respond to clicks
+        func onButtonClickHandler(control:Control, localLocation:Point) {
+
+            if let button = control as? Button {
+                let randomQuestion = Int.random(in: 1...49)
+                
+     //     insert(entity:background, at:.back)
+          insert(entity:QuestionDisplay(questionIndex: randomQuestion), at: .front)
+          insert(entity:QuestionDisplay(questionIndex: randomQuestion), at:. front)
+          //  print(randomQuestion)
+          
+
+          
+
+
+
+            }
+            //            button.labelString += "Hi"
+          
+        }
+        
+    
+        }
+
---
+++ KQuizTesting/Sources/ScenesShell/QuestionDisplay.swift
@@ -0,0 +1,27 @@
+import Igis
+import Scenes
+
+class QuestionDisplay: RenderableEntity {
+
+    let questionIndex: Int
+
+    init(questionIndex: Int) {
+        self.questionIndex = questionIndex
+        super.init(name:"QuestionDisplay")
+    }
+
+
+    override func setup(canvasSize:Size, canvas:Canvas) {
+
+        let questionOnScreen = Text(location:Point(x:400, y:200), text: quizQuestions[questionIndex].question)
+        let questionFillStyle = FillStyle(color:Color(.red))
+        let questionStrokeStyle = StrokeStyle(color:Color(.red))
+
+        for lineSpace in 0 ..< 4 {
+            let answerOnScreen = Text(location:Point(x:400, y:300+lineSpace*100), text: quizQuestions[questionIndex].answers[lineSpace])
+
+            canvas.render(questionFillStyle,questionStrokeStyle,questionOnScreen, answerOnScreen)
+
+        }
+    }
+}
